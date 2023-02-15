93e4a38d344a809b255fd5882363d378ceeec8ba
Webmapp dev server <devserver@webmapp.com>
2023-02-15 11:30:32 +0000

    Fix escapes il aliases.sh

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,5 +1,15 @@
-function geobox() {docker exec -it php81_$1 bash}
-function geobox_serve() {docker exec -it php81_$1 php artisan serve --host 0.0.0.0}
-function geobox_psql() {docker exec -it postgres_$1 psql -U $1}
-function geobox_deploy_and_serve() {docker exec -it php81_$1 bash scripts/deploy_local_with_seed_and_serve.sh}
-function geobox_install() {docker exec -it php81_$1 bash scripts/deploy_local_first.sh}
+function geobox() {
+	docker exec -it php81_$1 bash
+}
+function geobox_serve() {
+	docker exec -it php81_$1 php artisan serve --host 0.0.0.0
+}
+function geobox_psql() {
+	docker exec -it postgres_$1 psql -U $1
+}
+function geobox_deploy_and_serve() {
+	docker exec -it php81_$1 bash scripts/deploy_local_with_seed_and_serve.sh
+}
+function geobox_install() {
+	docker exec -it php81_$1 bash scripts/deploy_local_first.sh
+}
