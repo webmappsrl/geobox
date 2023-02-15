c07a2ef13eb19b791a84eecb726ee02f82dbdd4b
Alessio Piccioli <piccioli@netseven.it>
2023-02-15 07:43:29 +0100

    Added alaiases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,3 +1,10 @@
+function geobox() {docker exec -it php81_$1 bash}
+function geobox_serve() {docker exec -it php81_$1 php artisan serve --host 0.0.0.0}
+function geobox_psql() {docker exec -it postgres_$1 psql -U hoqu2}
+function geobox_deploy_and_serve() {docker exec -it php81_$1 bash scripts/deploy_local_with_seed_and_serve.sh}
+function geobox_install() {docker exec -it php81_$1 bash scripts/deploy_local_first.sh}
+
+
 alias hoqu2='docker exec -it php81_hoqu2 bash'
 alias hoqu2_serve='docker exec -it php81_hoqu2 php artisan serve --host 0.0.0.0'
 alias hoqu2_psql='docker exec -it postgres_hoqu2 psql -U hoqu2'
