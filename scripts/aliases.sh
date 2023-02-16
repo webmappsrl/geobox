5639de8edeba8a4f2448ac280255789c41982bdc
Alessio Piccioli <piccioli@netseven.it>
2023-02-16 11:26:09 +0100

    Updated aliases: added an help command that is called every time the commands geobox and geobox_* are called without a parameter

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,15 +1,40 @@
 function geobox() {
+	if [ -z "$1" ]; then geobox_help && return; fi
 	docker exec -it php81_$1 bash
 }
+
 function geobox_serve() {
+	if [ -z "$1" ]; then geobox_help && return; fi
 	docker exec -it php81_$1 php artisan serve --host 0.0.0.0
 }
+
 function geobox_psql() {
+	if [ -z "$1" ]; then geobox_help && return; fi
 	docker exec -it postgres_$1 psql -U $1
 }
+
 function geobox_deploy_and_serve() {
+	if [ -z "$1" ]; then geobox_help && return; fi
 	docker exec -it php81_$1 bash scripts/deploy_local_with_seed_and_serve.sh
 }
+
 function geobox_install() {
+	if [ -z "$1" ]; then geobox_help && return; fi
 	docker exec -it php81_$1 bash scripts/deploy_local_first.sh
+}
+
+function geobox_help() {
+echo ""
+echo "╔═╗╔═╗╔═╗╔╗ ╔═╗═╗ ╦"
+echo "║ ╦║╣ ║ ║╠╩╗║ ║╔╩╦╝"
+echo "╚═╝╚═╝╚═╝╚═╝╚═╝╩ ╚═"
+echo ""
+	echo "Available commands : the parameter [instance] refers to the name of the APP corresponding to the container"
+	echo ""
+	echo "geobox [instance] : enter the docker container"
+	echo "geobox_serve [instance] : start the web server (php artisan serve)"
+	echo "geobox_psql [instance] : enter psql"
+	echo "geobox_deploy_and_serve [instance] : deploy (composer install, migration) and serve"
+	echo "geobox_install [instance] : install (only first time)"
+	echo ""
 }
